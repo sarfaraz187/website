2c98749add46628892d75352a2f6f9dda7379797
sarfaraz187 <sarfarazmohammed187@gmail.com>
2025-01-04 13:25:48 +0100

    moved coverage test to a new script command

--- src/__tests__/components/Header.test.tsx
+++ src/__tests__/components/Header.test.tsx
@@ -1,14 +1,35 @@
 import Header from "@src/app/components/Header";
 import { render } from "@testing-library/react";
 import { BrowserRouter } from "react-router";
-import { describe, it } from "vitest";
+import { describe, expect, it, test } from "vitest";
+
+const userList = {
+  id: 1,
+  name: "Micheal",
+  email: "micheal@gmail.com",
+};
 
 describe("Header Component", () => {
-  it("renders the navigation menu links", () => {
+  it.concurrent("renders the navigation menu links", () => {
     render(
       <BrowserRouter>
         <Header />
       </BrowserRouter>
     );
+  });
+
+  test.concurrent("Contains correct user name", () => {
+    expect(userList.name).toBe("Micheal");
+    // expect(userList.email).toBe("Micheal");
+  });
+
+  test.todo("unimplemented test");
+
+  test.each([
+    { a: 1, b: 1, expected: 2 },
+    { a: 1, b: 2, expected: 3 },
+    { a: 2, b: 1, expected: 3 },
+  ])("add($a, $b) -> $expected", ({ a, b, expected }) => {
+    expect(a + b).toBe(expected);
   });
 });
