a7cea4e59f5ae19422883d5e1c9319c7a0cfa4c9
sarfaraz187 <sarfarazmohammed187@gmail.com>
2025-02-02 21:29:53 +0100

    analytics router moved to root layout

--- src/app/layout/RootLayout.tsx
+++ src/app/layout/RootLayout.tsx
@@ -2,10 +2,12 @@ import Footer from "@src/app/components/Footer";
 import Header from "@src/app/components/Header";
 import NavigationButton from "@src/app/components/ScrollNavigation";
 import ScrollToTop from "@src/hooks/useScrollToTop";
+import AnalyticsTracker from "@src/routes/AnalyticsTracker";
 import { Outlet } from "react-router";
 
 const RootLayout = () => (
   <>
+    <AnalyticsTracker />
     <NavigationButton />
     <ScrollToTop />
     <Header />
