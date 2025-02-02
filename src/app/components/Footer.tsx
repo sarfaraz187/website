7f74a5c2870bb5825f6f01a88af8980c7c8627c4
sarfaraz187 <sarfarazmohammed187@gmail.com>
2025-02-02 22:41:51 +0100

    resume added to footer from blob storage

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -13,9 +13,11 @@ const Footer = () => {
   return (
     <footer>
       {/* Contact Section */}
-      <section className="px-[13%] py-[6rem] bg-teaGreen dark:bg-gray-800">
+      <section className="px-[15%] py-[6rem] bg-teaGreen dark:bg-gray-800">
         <div className="grid grid-rows-1 md:grid-cols-2 align-center">
-          <h1 className="text-[4rem] md:text-[6rem] mb-6 md:mb-0 font-bold dark:text-white">{intl("footer.shallWe")}</h1>
+          <h1 className="text-[4rem] md:text-[6rem] mb-6 md:mb-0 font-bold dark:text-white">
+            {intl("footer.shallWe")}
+          </h1>
           <div className="align-center">
             <p className="text-lg mb-6">{intl("footer.description")}</p>
             <Button variant="primary" onClick={handleEmailClick}>
@@ -29,26 +31,46 @@ const Footer = () => {
           </div>
         </div>
       </section>
-      <section className="px-[13%] my-[6rem] md:my-[8rem]">
-        <div className="flex gap-8">
-          <UnderLineLink
-            href="https://www.linkedin.com/in/mohammed-sarfaraz-g/"
-            openInNewTab={true}
-          >
-            LinkedIn
-          </UnderLineLink>
-          <UnderLineLink
-            href="https://www.instagram.com/safaraz_44"
-            openInNewTab={true}
-          >
-            Instagram
-          </UnderLineLink>
-          <UnderLineLink
-            href="https://github.com/sarfaraz187"
-            openInNewTab={true}
-          >
-            Github
-          </UnderLineLink>
+      <section className="px-[13%] my-[4rem] md:my-[8rem]">
+        <div className="md:flex md:justify-end md:gap-[8rem] md:mr-[4rem]">
+          <div>
+            <h1 className="text-xl font-semibold mb-4 dark:text-white">
+              {intl("footer.contact")}
+            </h1>
+            <div className="inline-flex flex-col gap-1">
+              <UnderLineLink
+                href="https://www.linkedin.com/in/mohammed-sarfaraz-g/"
+                openInNewTab={true}
+              >
+                LinkedIn
+              </UnderLineLink>
+              <UnderLineLink
+                href="https://www.instagram.com/safaraz_44"
+                openInNewTab={true}
+              >
+                Instagram
+              </UnderLineLink>
+              <UnderLineLink
+                href="https://github.com/sarfaraz187"
+                openInNewTab={true}
+              >
+                Github
+              </UnderLineLink>
+            </div>
+          </div>
+          <div>
+            <h1 className="text-xl font-semibold mb-4 mt-6 md:mt-0 dark:text-white">
+              {intl("footer.general")}
+            </h1>
+            <div className="inline-flex flex-col">
+              <UnderLineLink
+                href="https://msarfaraz.blob.core.windows.net/portfolio-assets/mohammed_sarfaraz_cv.pdf?sp=r&st=2025-02-02T21:19:33Z&se=2026-02-03T05:19:33Z&spr=https&sv=2022-11-02&sr=b&sig=1DKxUVJA2inWCC3VUBcWxohnu6mrF4T%2BjlzMqNGZDAI%3D"
+                openInNewTab={true}
+              >
+                Resume
+              </UnderLineLink>
+            </div>
+          </div>
         </div>
       </section>
     </footer>
--- src/app/components/UnderlineLink.tsx
+++ src/app/components/UnderlineLink.tsx
@@ -14,8 +14,7 @@ const UnderLineLink = ({
     <a
       href={href}
       {...targetProps}
-      className={`text-lg font-medium pb-2 relative text-gray hover:text-black dark:text-slate dark:hover:text-seaShell after:bg-nightRider after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:dark:bg-teaGreen after:transition-all after:duration-300 hover:after:w-full ${className}`}
-    >
+      className={`text-lg font-medium pb-2 relative text-gray hover:text-black dark:text-slate dark:hover:text-seaShell after:bg-nightRider after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:dark:bg-teaGreen after:transition-all after:duration-300 hover:after:w-full inline-block ${className}`}>
       {children}
     </a>
   );
